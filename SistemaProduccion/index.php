d66e14d0e5c28573e010b1fbb73bd172afdd1749
Cristian amezcua <momentousjoker@gmail.com>
2022-11-25 22:36:09 -0600

    NV1.33

--- SistemaProduccion/index.php
+++ SistemaProduccion/index.php
@@ -84,7 +84,10 @@
         </div>
     </div>
 
-
+<script>
+    alert('Recordatorios')
+    alert('RECUERDA EN COMPRA DE INSUMO,VAles de salida y devoluciones que tengan lo mismos datos para insumos');
+</script>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
