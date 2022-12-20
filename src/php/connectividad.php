3cab07d92746efd25d92941d92b1bb66d9fbad39
Cristian amezcua <momentousjoker@gmail.com>
2022-12-19 21:17:34 -0600

    NV1.55

--- src/php/connectividad.php
+++ src/php/connectividad.php
@@ -1,8 +1,9 @@
 <?php
 class DB_Connect {
-    const URL = 'mysql:host=localhost;dbname=u517350403_apeajalvivero';
-    const USERNAME = 'u517350403_apeajalvivero';
-    const PASSWORD = 'S|1#q&5Jt9/';
+    const URL = 'mysql:host=mysql-recidenciacyj.alwaysdata.net;dbname=recidenciacyj_apeajal';
+    const USERNAME = '279932_jessica';
+    const PASSWORD = 'BTS2103';
+
     const OPCION = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
     var $connection=null;
 
@@ -31,7 +32,7 @@ class DB_Connect {
     public function connect_externa() 
     {
         try {
-            $this->connection = new PDO("mysql:host=localhost;dbname=u517350403_apeajal", "u517350403_apeajalweb", "p^hbc&B8", [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
+            $this->connection = new PDO("mysql:host=mysql-recidenciacyj.alwaysdata.net;dbname=recidenciacyj_bts", "279932_jessica", "BTS2103", [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
             $this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } 
