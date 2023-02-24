d5aeaeebd5b12064645c2920af9bc85e1afff0d7
Cristian amezcua <momentousjoker@gmail.com>
2023-02-23 19:33:40 -0600

    NV1.63
    Fin de proyecto

--- src/php/connectividad.php
+++ src/php/connectividad.php
@@ -19,7 +19,7 @@ class DB_Connect {
                 $this->connection = new PDO('mysql:host=mysql-recidenciacyj.alwaysdata.net;dbname=recidenciacyj_apeajal', '279932_jessica', 'BTS2103', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
             }else{
                 $this->table_schema="u517350403_apeajalvivero";
-                $this->connection = new PDO('mysql:host=localhost;dbname=u517350403_apeajalvivero', 'u517350403_apeajalvivero', 'S|1#q&5Jt9/', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
+                $this->connection = new PDO('mysql:host=localhost;dbname=u517350403_apeajalvivero', 'u517350403_apeajalvivero', 'Apeajalvivero1', [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
             }
             $this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
